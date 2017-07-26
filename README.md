4a432b1dc05fd890d710d313cc4eebfdd4409ad1
Kevin <kevinwang2400@gmail.com>
2017-07-26 16:21:24 -0700

    set up postgres

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 
 
-python3 virtual env
+### TODOS
+[ ] install shell plus
+
+### python3 virtual env
+
 ```
-virtualenv -p python3 envname
+virtualenv -p python3 avalon
 ```
 
 
--- avalon/avalon/settings.py
+++ avalon/avalon/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'avalon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'avalon',
+        'USER': os.environ.get('AV_DB_USER'),
+        'PASSWORD': os.environ.get('AV_DB_PW'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
