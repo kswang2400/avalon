a0c4226c25d5cc86ff2cea193aa4bb457c1e8d94
Kevin Wang <kevinwang2400@gmail.com>
2018-03-24 15:52:21 -0700

    delete ds store, remove virtualenv stuff from deploy, already in wsgi conf (#25)

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
+#!/bin/bash
+
 # preamble
-deactivate
 sudo service nginx stop
 
-# update version control
+# update codebase
+# KW: TODO status checks
 cd /home/ec2-user/avalon_game/webapp
 git stash
 git checkout master
 git fetch
 git merge origin master
-
-# virtualenv
-source /home/ec2-user/venv/avalon/bin/activate
 
 # run uwsgi
 # KW: TODO set up uwsgi emperor for zero downtime
